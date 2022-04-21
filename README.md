15c9b54af815201b0e9c8d4efa541ef29e227d7f
Graham Nelson <graham.nelson@mod-langs.ox.ac.uk>
2022-04-22 00:08:01 +0100

    Migrated to new makescript format

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Intest 2.0
 
-v2.0-alpha.1+1A27 'The Remembering' (15 April 2022)
+v2.0-alpha.1+1A28 'The Remembering' (22 April 2022)
 
 ## About Intest
 
--- scripts/first.sh
+++ scripts/first.sh
@@ -1,6 +1,6 @@
 echo "(A script to make a first build of Intest)"
 echo "(Step 1 of 2: making the makefile)"
-if ! ( inweb/Tangled/inweb intest -prototype intest/scripts/makescript.txt -makefile intest/intest.mk; ) then
+if ! ( inweb/Tangled/inweb intest -prototype intest/scripts/intest.mkscript -makefile intest/intest.mk; ) then
 	echo "(Okay, so that failed. Have you installed and built Inweb?)"
 	exit 1
 fi
